e54f78800a86db3afd1919b8a9030b3110db54d1
Matteo Landi <matteo@matteolandi.net>
2019-11-04 00:00:03 +0100

    Fucking initialize `argv`

--- src/main.lisp
+++ src/main.lisp
@@ -88,7 +88,7 @@
          :arg-parser #'identity
          :meta-var "SELF"))
 
-(defun parse-opts ()
+(defun parse-opts (&optional (argv (opts:argv)))
   (multiple-value-bind (options)
       (handler-case
           (handler-bind ((opts:missing-required-option (lambda (condition)
@@ -201,3 +201,34 @@
 (defun toplevel()
   (parse-opts)
   (process-input))
+
+;;; REPL ----------------------------------------------------------------------
+
+#+NIL
+(setf *version* "0.0.1"
+      *title* "Matteo Landi's blog"
+      *link* "https://matteolandi.net/.plan"
+      *generator* (format NIL "plan-rss ~a" *version*)
+      *image* "https://matteolandi.net/static/avatar-144.jpg"
+      *atom-link-self* "https://matteolandi.net/plan.xml")
+
+#+NIL
+(defun fake-input-stream ()
+  (make-string-input-stream "This is my log ...
+
+When I accomplish something, I write a * line that day.
+
+Whenever a bug / missing feature / idea is mentioned during the day and I don't fix it, I make a note of it and mark it with ?.  Some things get noted many times before they get fixed.
+
+Occasionally I go back through the old notes and mark with a + the things I have since fixed, and with a ~ the things I have since lost interest in.
+
+--- Matteo Landi
+
+# 2019-11-01
+* xml-emitter: Add support for guid isPermaLink=false (https://github.com/VitoVan/xml-emitter/pull/3)
+* xml-emitter: Add support for atom:link with rel=\"self\" (https://github.com/VitoVan/xml-emitter/pull/4)
+
+# 2019-10-30
+Finally A/I came back online, and I was finally able to create a request for a mailing list (to use it with the other college friends).  Anyway, the request has been created, so hopefully over the following days we will hear back from them...stay tuned!
+
+"))
